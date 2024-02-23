69c9762488ddb357ca4303e5ed164c2ecafe4e4f
Kaczmarczykk97 <kamil.kaczmarczykk@gmail.com>
2024-02-23 09:59:55 +0100

    improve design and functions AbouUs

--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 import classes from "./AboutUs.module.css";
 
 import { FaTrophy, FaChartLine, FaPhoneAlt } from "react-icons/fa";
 import { MdEngineering } from "react-icons/md";
 import { TbPigMoney } from "react-icons/tb";
+import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io";
 
 import experience_img from "../Imgs/experience_img.png";
 import services_crew_img from "../Imgs/aboutUsWrapper_services_crew.jpg";
@@ -12,6 +15,45 @@ import co_installation_img from "../Imgs/co_installation.png";
 import cwu_installation_img from "../Imgs/cwu_installation.png";
 
 function AboutUs() {
+  const [moreAboutCrew, setMoreAboutCrew] = useState(false);
+
+  const [moreAboutHeatPump, setMoreAboutHeatPump] = useState(false);
+  const [moreAboutBoilerRoom, setMoreAboutBoilerRoom] = useState(false);
+  const [moreAboutCOInstallation, setMoreAboutCOInstallation] = useState(false);
+  const [moreAboutCWUInstallation, setMoreAboutCWUInstallation] =
+    useState(false);
+
+  function openMoreAboutCrewHandler() {
+    setMoreAboutCrew((prev) => !prev);
+  }
+
+  function openMoreAboutServicesHandler(e) {
+    if (
+      e.target.dataset.id === "heat-pump" ||
+      e.target.closest("button").dataset.id === "heat-pump"
+    ) {
+      setMoreAboutHeatPump((prev) => !prev);
+    }
+    if (
+      e.target.dataset.id === "boiler-room" ||
+      e.target.closest("button").dataset.id === "boiler-room"
+    ) {
+      setMoreAboutBoilerRoom((prev) => !prev);
+    }
+    if (
+      e.target.dataset.id === "co-installation" ||
+      e.target.closest("button").dataset.id === "co-installation"
+    ) {
+      setMoreAboutCOInstallation((prev) => !prev);
+    }
+    if (
+      e.target.dataset.id === "cwu-installation" ||
+      e.target.closest("button").dataset.id === "cwu-installation"
+    ) {
+      setMoreAboutCWUInstallation((prev) => !prev);
+    }
+  }
+
   return (
     <div className={classes["aboutUsBox"]} id="section-about">
       <div className={classes["headingBox"]}>
@@ -74,50 +116,149 @@ function AboutUs() {
             się, że możemy uczynić Twoje marzenia o nowoczesnej kotłowni lub
             kompleksowej instalacji rzeczywistością.
           </p>
-          <p>
-            Dlaczego warto skorzystać z naszych usług? Nasze 30-letnie
-            doświadczenie przekłada się na solidność i profesjonalizm. Oferujemy
-            nie tylko instalacje, ale także partnerstwo oparte na zaufaniu.
-            Nasza oferta jest dopasowana do Twoich indywidualnych potrzeb, a
-            zespół służy fachowym doradztwem i kompleksową obsługą. Z nami ciesz
-            się ciepłem w domu, oszczędzając rozsądne pieniądze.
-          </p>
+          {moreAboutCrew && (
+            <p>
+              Dlaczego warto skorzystać z naszych usług? Nasze 30-letnie
+              doświadczenie przekłada się na solidność i profesjonalizm.
+              Oferujemy nie tylko instalacje, ale także partnerstwo oparte na
+              zaufaniu. Nasza oferta jest dopasowana do Twoich indywidualnych
+              potrzeb, a zespół służy fachowym doradztwem i kompleksową obsługą.
+              Z nami ciesz się ciepłem w domu, oszczędzając rozsądne pieniądze.
+            </p>
+          )}
+          <button onClick={openMoreAboutCrewHandler}>
+            {moreAboutCrew ? (
+              <IoMdArrowDropup className={classes["closeArr"]} />
+            ) : (
+              "Dowiedz się więcej"
+            )}
+          </button>
         </div>
         <div className={classes["aboutUsWrapper--services-wrapper"]}>
           <div className={classes["aboutUsWrapper--service-box--row"]}>
-            <div className={classes["service-box"]}>
+            <div
+              className={classes["service-box"]}
+              data-id="heatPump"
+              style={
+                moreAboutHeatPump
+                  ? { height: "auto" }
+                  : { height: "max-content" }
+              }
+            >
+              <div className={classes["curtain"]}>
+                <button
+                  onClick={openMoreAboutServicesHandler}
+                  data-id="heat-pump"
+                >
+                  Pompy ciepła
+                  {moreAboutHeatPump ? (
+                    <IoMdArrowDropup className={classes["curtain-arr"]} />
+                  ) : (
+                    <IoMdArrowDropdown className={classes["curtain-arr"]} />
+                  )}
+                </button>
+              </div>
               <img src={heat_pump_img} alt="heat pump" />
-              <p>
-                Kompletne rozwiązania z zakresu pomp ciepła, oferując naszym
-                klientom profesjonalną wycenę, fachowy montaż oraz wsparcie w
-                uzyskaniu dostępnych dofinansowań.
-              </p>
+              {moreAboutHeatPump && (
+                <p>
+                  Kompletne rozwiązania z zakresu pomp ciepła, oferując naszym
+                  klientom profesjonalną wycenę, fachowy montaż oraz wsparcie w
+                  uzyskaniu dostępnych dofinansowań.
+                </p>
+              )}
             </div>
-            <div className={classes["service-box"]}>
+            <div
+              className={classes["service-box"]}
+              style={
+                moreAboutBoilerRoom
+                  ? { height: "auto" }
+                  : { height: "max-content" }
+              }
+            >
+              <div className={classes["curtain"]}>
+                <button
+                  onClick={openMoreAboutServicesHandler}
+                  data-id="boiler-room"
+                >
+                  Kotłownie
+                  {moreAboutBoilerRoom ? (
+                    <IoMdArrowDropup className={classes["curtain-arr"]} />
+                  ) : (
+                    <IoMdArrowDropdown className={classes["curtain-arr"]} />
+                  )}
+                </button>
+              </div>
               <img src={boiler_room_img} alt="boiler room" />
-              <p>
-                Kompleksowo projektujemy i wykonujemy nowoczesne kotłownie,
-                zapewniając naszym klientom pełną gamę usług. Nasza oferta
-                obejmuje zaprojektowanie instalacji oraz montaż.
-              </p>
+              {moreAboutBoilerRoom && (
+                <p>
+                  Kompleksowo projektujemy i wykonujemy nowoczesne kotłownie,
+                  zapewniając naszym klientom pełną gamę usług. Nasza oferta
+                  obejmuje zaprojektowanie instalacji oraz montaż.
+                </p>
+              )}
             </div>
           </div>
           <div className={classes["aboutUsWrapper--service-box--row"]}>
-            <div className={classes["service-box"]}>
+            <div
+              className={classes["service-box"]}
+              style={
+                moreAboutCOInstallation
+                  ? { height: "auto" }
+                  : { height: "max-content" }
+              }
+            >
+              <div className={classes["curtain"]}>
+                <button
+                  onClick={openMoreAboutServicesHandler}
+                  data-id="co-installation"
+                >
+                  Instalacje C.O.
+                  {moreAboutCOInstallation ? (
+                    <IoMdArrowDropup className={classes["curtain-arr"]} />
+                  ) : (
+                    <IoMdArrowDropdown className={classes["curtain-arr"]} />
+                  )}
+                </button>
+              </div>
               <img src={co_installation_img} alt="co installation" />
-              <p>
-                Profesjonalny montaż kotłów i wymianę pionów grzewczych.
-                Projektujemy instalację CO oraz wykonujemy ogrzewanie podłogowe,
-                zapewniając klientom komfort cieplny i energooszczędność.
-              </p>
+              {moreAboutCOInstallation && (
+                <p>
+                  Profesjonalny montaż kotłów i wymianę pionów grzewczych.
+                  Projektujemy instalację CO oraz wykonujemy ogrzewanie
+                  podłogowe, zapewniając klientom komfort cieplny i
+                  energooszczędność.
+                </p>
+              )}
             </div>
-            <div className={classes["service-box"]}>
+            <div
+              className={classes["service-box"]}
+              style={
+                moreAboutCWUInstallation
+                  ? { height: "auto" }
+                  : { height: "max-content" }
+              }
+            >
+              <div className={classes["curtain"]}>
+                <button
+                  onClick={openMoreAboutServicesHandler}
+                  data-id="cwu-installation"
+                >
+                  Instalacje C.W.U.
+                  {moreAboutCWUInstallation ? (
+                    <IoMdArrowDropup className={classes["curtain-arr"]} />
+                  ) : (
+                    <IoMdArrowDropdown className={classes["curtain-arr"]} />
+                  )}
+                </button>
+              </div>
               <img src={cwu_installation_img} alt="cwu installation" />
-              <p>
-                Nasze instalacje C.W.U. obejmują staranne przyłączenie do sieci
-                wodociągowej, zastosowanie nowoczesnych bojlerów i
-                zabezpieczenia przed utratą ciepła.
-              </p>
+              {moreAboutCWUInstallation && (
+                <p>
+                  Nasze instalacje C.W.U. obejmują staranne przyłączenie do
+                  sieci wodociągowej, zastosowanie nowoczesnych bojlerów i
+                  zabezpieczenia przed utratą ciepła.
+                </p>
+              )}
             </div>
           </div>
         </div>
