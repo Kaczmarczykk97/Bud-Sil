800fd9efba4af9ac4cc859286ff1f45a73827443
Kaczmarczykk97 <kamil.kaczmarczykk@gmail.com>
2024-02-28 10:37:17 +0100

    responsive Location

--- src/Components/Location/Location.js
+++ src/Components/Location/Location.js
@@ -34,9 +34,7 @@ function Location() {
           <iframe
             title="Google Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1259.3614826051055!2d17.46609153725851!3d50.854814187347436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47103020f44043f3%3A0xb141d95e21c33590!2sBud-Sil.%20ZUH.%20Silarski%20W.!5e0!3m2!1spl!2spl!4v1708613197804!5m2!1spl!2spl"
-            width="550"
-            height="400"
-            style={{ border: 0, marginLeft: "10rem" }}
+            style={{ border: 0, width: "100%", height: "100%" }}
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
